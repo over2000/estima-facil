67ecf8aaafbee6e912c4fa8a6ca11a276fb59d9c
Daniel Vitor Marinho Lisboa (G4F) <daniel.v.lisboa@itamaraty.gov.br>
2023-06-15 17:07:23 -0300

    recebendo tipo de item a ser adicionado

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 import { Grid, TextField, Autocomplete, Chip, Typography } from '@mui/material';
 
-export default function Item() {
+export default function Item( {tipoItem} ) {
+
+  console.log(tipoItem)
 
 
   const fakeDataFromServer = [
     { key: 0, value: "P.1 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end. Programação completa, incluindo validação do campo, sanitização das “strings” etc." },
     { key: 1, value: "P.2 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end, baseada em programação semelhante já existente ou CRUD. Programação completa, incluindo validação do campo, sanitização das “strings” etc." }
   ];
-  
 
+  const fakeDataFromServer2 = [
+    { key: 0, value: "Deploy" },
+    { key: 1, value: "Deply2222" }
+  ];
+  
   const [options, setOptions] = useState([]);
 
   useEffect(() => {
     // here goes your fetch call
     // when response arrives -
-    setOptions(fakeDataFromServer);
+    if(tipoItem === 'Banco de dados'){
+      setOptions(fakeDataFromServer);
+    }
+    if(tipoItem === 'Deploy'){
+      setOptions(fakeDataFromServer2);
+    }
+    
   }, []);
 
   return (
