f5705de32b66d932a1f4afd0e0f958b3ce6facb4
over2000 <danielvitorcg@gmail.com>
2023-07-12 23:48:10 -0300

    app js revert changes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
 import React, { useState } from 'react';
-import { Grid, Button, IconButton, Select, MenuItem, FormControl, TextField, Typography } from '@mui/material';
+import { Grid, Button, IconButton,Select, MenuItem, FormControl, TextField } from '@mui/material';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import Item from './Components/Item';
 
 function App() {
+
   const [selectedType, setSelectedType] = useState('');
-  const handleSelectType = event => {
+
+  const handleSelectType = (event) => {
     setSelectedType(event.target.value);
   };
 
   const [items, setItems] = useState([]);
 
   const handleAddItem = () => {
-    setItems(prevItems => [
-      ...prevItems,
-      { tipoItem: selectedType, selectedItems: [], totalPoints: 0 }
-    ]);
+    setItems(prevItems => [...prevItems, <Item tipoItem={selectedType} key={prevItems.length} />]);
   };
 
   const handleDeleteItem = index => {
-    setItems(prevItems => prevItems.filter((_, i) => i !== index));
+    setItems(prevItems => prevItems.filter((item, i) => i !== index));
   };
-
-  const handleItemsChange = (index, newItems) => {
-    setItems(prevItems =>
-      prevItems.map((item, i) => (i === index ? { ...item, selectedItems: newItems } : item))
-    );
-  };
-
-  const handleTotalPointsChange = (index, points) => {
-    setItems(prevItems =>
-      prevItems.map((item, i) => (i === index ? { ...item, totalPoints: points } : item))
-    );
-  };
-
-  const calculateTotalPoints = () => {
-    let sum = 0;
-    items.forEach(item => {
-      sum += item.totalPoints;
-    });
-    return sum;
-  };
-
-  console.log('e');
 
   return (
     <div style={{ width: '100%' }}>
-      <Grid container spacing={2} my={5} direction="column" alignItems="center" justifyContent="center">
+      <Grid
+        container
+        spacing={2}
+        my={5}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
         {items.map((item, index) => (
           <Grid item key={index}>
-            <Item
-              tipoItem={item.tipoItem}
-              selectedItems={item.selectedItems}
-              onItemsChange={newItems => handleItemsChange(index, newItems)}
-              onTotalPointsChange={points => handleTotalPointsChange(index, points)}
-            />
+            {item}
             <IconButton variant="contained" onClick={() => handleDeleteItem(index)}>
-              <RemoveCircleOutlineIcon />
+                <RemoveCircleOutlineIcon />
             </IconButton>
           </Grid>
         ))}
@@ -69,7 +48,9 @@ function App() {
               id="component-type"
               value={selectedType}
               onChange={handleSelectType}
-              renderInput={params => <TextField {...params} label="Atividade" variant="outlined" />}
+              renderInput={(params) => (
+                <TextField {...params} label="Atividade" variant="outlined" />
+              )}
             >
               <MenuItem value="Frontend">Frontend</MenuItem>
               <MenuItem value="Backend">Backend</MenuItem>
@@ -80,15 +61,15 @@ function App() {
         </Grid>
         {selectedType && (
           <Grid item>
-            <Button variant="contained" color="primary" onClick={handleAddItem}>
+            <Button variant="contained" color="primary" onClick={() => handleAddItem(selectedType)}>
               Adicionar Item {selectedType}
             </Button>
           </Grid>
         )}
 
-        <Grid item>
-          <Typography>Total de pontos: {totalPoints}</Typography>
-        </Grid>
+
+
+
       </Grid>
     </div>
   );
