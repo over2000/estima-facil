77b8978227e39b2342dcc6b8b43751edfbb38e36
Daniel Vitor Marinho Lisboa (G4F) <daniel.v.lisboa@itamaraty.gov.br>
2023-07-17 21:34:54 -0300

    ajuste no objeto

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,31 +3,31 @@ import { Grid, TextField, Autocomplete, Typography } from '@mui/material';
 
 export default function Item({ id, tipoItem, items, setItems }) {
   const frontendOptions = [
-    { key: 0, value: "P.1 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end. Programação completa, incluindo validação do campo, sanitização das “strings” etc.", points: 5 },
-    { key: 1, value: "P.2 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end, baseada em programação semelhante já existente ou CRUD. Programação completa, incluindo validação do campo, sanitização das “strings” etc.", points: 5 },
-    { key: 2, value: "P.3 - Programação de 1 funcionalidade simples, no front-end.", points: 5 },
-    { key: 3, value: "P.4 - Programação de validação de campos de formulário, no front-end, com mensagens ao usuário.", points: 5 },
-    { key: 4, value: "P.5 - Programação de funcionalidade no front-end, completa, com tratamento de dados, validação, submissão ao back-end e tratamento e incorporação do retorno.", points: 5 },
-    { key: 5, value: "T.1 - Teste e Debug funcionalidades novas.", points: 5 },
-    { key: 6, value: "T.2 - Teste e Debug para manutenção.", points: 5 },
-    { key: 7, value: "T.3 - Teste e Debug para manutenção, de sistemas críticos, com dependências de outros sistemas.", points: 5 },
+    { key: 0, value: "P.1 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end. Programação completa, incluindo validação do campo, sanitização das “strings” etc. - 4 UST's", points: 4 },
+    { key: 1, value: "P.2 - Programação de 1 operação de banco (criação, leitura, atualização, remoção) no back-end, com dados submetidos pelo front-end, baseada em programação semelhante já existente ou CRUD. Programação completa, incluindo validação do campo, sanitização das “strings” etc. - 2 UST's", points: 2 },
+    { key: 2, value: "P.3 - Programação de 1 funcionalidade simples, no front-end. - 2 UST's", points: 2 },
+    { key: 3, value: "P.4 - Programação de validação de campos de formulário, no front-end, com mensagens ao usuário. 0,5 UST's", points: 0.5 },
+    { key: 4, value: "P.5 - Programação de funcionalidade no front-end, completa, com tratamento de dados, validação, submissão ao back-end e tratamento e incorporação do retorno. - 4 UST's ", points: 4 },
+    { key: 5, value: "T.1 - Teste e Debug funcionalidades novas. - 1 UST's", points: 1 },
+    { key: 6, value: "T.2 - Teste e Debug para manutenção. - 0,5 UST's", points: 0.5 },
+    { key: 7, value: "T.3 - Teste e Debug para manutenção, de sistemas críticos, com dependências de outros sistemas. - 2 UST's", points: 2 },
   ];
 
   const backendOptions = [
-    { key: 0, value: "P.6 - Programação de teste unitário. A programação de teste unitário será remunerada com o mesmo número de USTs da função/método/serviço que esse teste visa a testar.", points: 5 },
-    { key: 1, value: "P.7 - Alteração de programação de operação de banco. Esta atividade envolve todo o escopo da alteração (banco e programação).", points: 5 },
-    { key: 2, value: "P.8 - Alteração pontual de funcionalidade existente, no back-end para arquitetura orientada a  serviço ou para aplicações monólitos.", points: 5 },
-    { key: 3, value: "P.9 - Programação de método para gravação de logs, com teste e debug incluídos.", points: 5 },
-    { key: 4, value: "T.1 - Teste e Debug funcionalidades novas.", points: 5 },
-    { key: 5, value: "T.2 - Teste e Debug para manutenção.", points: 5 },
-    { key: 6, value: "T.3 - Teste e Debug para manutenção, de sistemas críticos, com dependências de outros sistemas.", points: 5 },
+    { key: 0, value: "P.6 - Programação de teste unitário. A programação de teste unitário será remunerada com o mesmo número de USTs da função/método/serviço que esse teste visa a testar. - 4 UST's", points: 4 },
+    { key: 1, value: "P.7 - Alteração de programação de operação de banco. Esta atividade envolve todo o escopo da alteração (banco e programação). - 2 UST's", points: 2 },
+    { key: 2, value: "P.8 - Alteração pontual de funcionalidade existente, no back-end para arquitetura orientada a  serviço ou para aplicações monólitos. 0,5 UST's", points: 0.5 },
+    { key: 3, value: "P.9 - Programação de método para gravação de logs, com teste e debug incluídos. 2 UST's", points: 2 },
+    { key: 4, value: "T.1 - Teste e Debug funcionalidades novas. - 1 UST's", points: 1 },
+    { key: 5, value: "T.2 - Teste e Debug para manutenção. - 0,5 UST's", points: 0.5 },
+    { key: 6, value: "T.3 - Teste e Debug para manutenção, de sistemas críticos, com dependências de outros sistemas. - 2 UST's", points: 2 },
   ];
 
   const bancoOptions = [
-    { key: 0, value: "DB.1 - Criação de script para operação direta em banco de dados, incluindo até 10 sub-variações em seus parâmetros.", points: 5 },
-    { key: 1, value: "DB.2 - Teste de script de operação direta em banco de dados (apenas quando necessário).", points: 5 },
-    { key: 2, value: "DB.3 - Alteração em tabela única - DDL - ALTER", points: 5 },
-    { key: 3, value: "DB.4 - Criação de script de DDL (CREATE, ALTER E DROP)", points: 5 },
+    { key: 0, value: "DB.1 - Criação de script para operação direta em banco de dados, incluindo até 10 sub-variações em seus parâmetros. - 2 UST's", points: 2 },
+    { key: 1, value: "DB.2 - Teste de script de operação direta em banco de dados (apenas quando necessário). 0,5 UST's", points: 0.5 },
+    { key: 2, value: "DB.3 - Alteração em tabela única - DDL - ALTER - 0,5 UST's", points: 0.5 },
+    { key: 3, value: "DB.4 - Criação de script de DDL (CREATE, ALTER E DROP) - 0,5 UST's", points: 0.5 },
   ];
 
   const [selectedItems, setSelectedItems] = useState([]);
